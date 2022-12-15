09e030621b9f939c37e379b7c460fa58d6c2e3a9
Sayad Uddin Tahsin <89304780+Sayad-Uddin-Tahsin@users.noreply.github.com>
2022-12-15 21:52:07 +0600

    Create 8. XML2 - Find the Maximum Depth.py

--- Challenges/8. XML2 - Find the Maximum Depth.py
+++ Challenges/8. XML2 - Find the Maximum Depth.py
@@ -4,11 +4,8 @@ maxdepth = 0
 def depth(elem, level):
     global maxdepth
     maxdepth = level + 1
-    
-    # check childrens of current node
     for child in elem:
         maxdepth = max(maxdepth, depth(child, level + 1))
-        # maxdepth = max(maxdepth, depth(child, level + 1))
     return maxdepth
 
 
